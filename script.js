ef62ee50d435dd2d01ac6ad74a7caa166df44d78
rameshghule11 <rameshghule112002@gmail.com>
2024-12-14 09:02:30 -0800

    script file modify

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function inc(event){
     let quantity = parseInt(quantityContainer.innerText);
     quantityContainer.innerText = ++quantity;
 }
+
+function dec(event){
+    const quantityContainer = event.target.parentElement.querySelector(".quantity");
+    let quantity = parseInt(quantityContainer.innerText);
+    if (quantity > 1) {
+        quantityContainer.innerText = --quantity;
+    }
+}
